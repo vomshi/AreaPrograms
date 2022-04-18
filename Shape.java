49d29de31229cc941c8b8c66ca8f7f861cc93cbc
Vomshi P R <48349354+vomshi@users.noreply.github.com>
2022-04-18 12:29:28 +0530

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,16 @@
+package coding;
+
+public abstract class shape {
+
+//    Create a class of Shape which can help to instantiate objects.
+//    The objects will be square, rectangle, circle, cube, sphere
+//    each object will need to have color and dimensions
+//    you need to be able to evaluate area for 2 dimension objects
+//    you need to be able to evaluate volume for 3 dimensional objects
+
+    float length;
+    String colour;
+   abstract void area();
+
+
+}
